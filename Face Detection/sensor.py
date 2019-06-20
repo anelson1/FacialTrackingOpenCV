db04ceae3926463e891a64d321c35c4c13a0f2df
Angelo Nelson <angelonelson@outlook.com>
2019-06-19 20:49:31 -0700

    Tuned facial recognition and eye recognition and added seperate display windows for each

--- Face Detection/sensor.py
+++ Face Detection/sensor.py
@@ -9,22 +9,30 @@ r=255
 g=0
 b=0
 count = 0
-while(True):    
+roi_color = None
+roi_eyes = None
+while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    faces = face_cascade.detectMultiScale(gray, 1.5, 3) 
+    faces = face_cascade.detectMultiScale(gray, 1.1, 10)
     for (x,y,w,h) in faces: 
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)  
         roi_gray = gray[y:y+h, x:x+w] 
         roi_color = frame[y:y+h, x:x+w]
-        cv2.imwrite(".\output\image"+str(count)+".png",roi_color)
-        count += 1
-        eyes = eye_cascade.detectMultiScale(roi_gray,1.5,10)
+        #cv2.imwrite(".\output\image"+str(count)+".png",roi_color)
+        #count += 1
+        eyes = eye_cascade.detectMultiScale(roi_gray,1.1,10)
         for (ex,ey,ew,eh) in eyes: 
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2) 
-  
-
-    cv2.imshow('Huge Data',frame)
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
+            print(ex,ey,ex+ew,ey+eh)
+            roi_eyes = roi_color[ey:ey+eh,ex:ex+ew]
+            cv2.imwrite(".\output\image" + str(count) + ".png", roi_eyes)
+            count += 1
+    cv2.imshow("Full",frame)
+    if roi_color is not None:
+        cv2.imshow('Face',roi_color)
+    if roi_eyes is not None:
+        cv2.imshow('Eyes',roi_eyes)
 
     # Display the resulting frame
     if cv2.waitKey(1) & 0xFF == ord('q'):
